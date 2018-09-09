f4272b393e47c23d1f97d07a339411ab2cf03f88
Keeley Hoek <keeley@hoek.io>
2018-09-09 21:06:29 +1000

    Provide default definition of "k.find_separating_hyperplane"

--- src/libk/extras.lean
+++ src/libk/extras.lean
@@ -11,12 +11,14 @@ namespace k
 
 -- Functions which use IO types go via a call like this. (Routed through the instance
 -- of vm_extra_io.)
--- TODO Having "noncomputable" here is a compromise with the compiler.
+-- FIXME Having "noncomputable" here is a compromise with the compiler.
+-- If we get serious about IO extensions, this will have to change.
 noncomputable def greet : io unit := vm_extra_io.greet io_core
 
 -- Functions which do not use IO types are just declared like this.
-constant find_separating_hyperplane {dim : ℕ} (a_vects : list (array dim ℕ))
-    (b_vects : list (array dim ℕ)) : (array dim ℤ) × ℤ
+def find_separating_hyperplane {dim : ℕ} (a_vects : list (array dim ℕ))
+    (b_vects : list (array dim ℕ)) : (array dim ℤ) × ℤ :=
+  (mk_array dim 0, 0)
 -- TODO use an honest, raw array-backed data type (the built-in array/parray isn't
 -- do this and frankly that seems really pointless)
 
